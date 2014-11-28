ae912e560cc9ccc984cb1c8386691a823f7e0952
Ryan Baumann <ti.teg.tnod.I@gmail.com>
2014-11-28 11:20:24 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+textstuff
+=========
+
+I've modified insaneKane's text stuff he has for the 3DS, but I made it work for ctrulib. I've also added in more character support like .,:!?1234567890, fixed bugs, and cleaned it up a bit (with more cleaning to come).
+
+Setup
+
+1. Just follow the instructions to setup your development environment here: http://3dbrew.org/wiki/Setting_up_Development_Environment
+2. Copy or link "letters.h", "memory.h", and "memory.c" to example/source.
+3. Go ahead and compile the example.
+
+
+Credits
+
+* insaneKane - The original code
+* titegtnodI - Modifications for ctrulib
---
+++ example/source/main.c
@@ -0,0 +1,44 @@
+#include <3ds.h>
+#include "memory.h"
+
+int main()
+{
+	// Initialize services
+	srvInit();
+	aptInit();
+	hidInit(NULL);
+	gfxInit();
+	//gfxSet3D(true); // uncomment if using stereoscopic 3D
+
+	char* a = "hello world";
+	
+	// Main loop
+	while (aptMainLoop())
+	{
+		gspWaitForVBlank();
+		hidScanInput();
+
+		// Your code goes here
+
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+		// Example rendering code that displays some text
+		// Please note that the 3DS screens are sideways (thus 240x400 and 240x320)
+		u8* fb = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+		memset(fb, 0, 240*400*3);
+		paint_word(fb, a, 40, 40, 255, 255, 255);
+
+		// Flush and swap framebuffers
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+	}
+
+	// Exit services
+	gfxExit();
+	hidExit();
+	aptExit();
+	srvExit();
+	return 0;
+}
